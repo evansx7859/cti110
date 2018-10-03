e6d18bfbee7f35207df8519262dbd705962627ed
evansx7859 <43824846+evansx7859@users.noreply.github.com>
2018-10-03 13:51:43 -0400

    Add files via upload

---
+++ P2HW2_MaleFemale_Percentage_XzavianEvans.py
@@ -0,0 +1,15 @@
+# A brief description of the project
+# Oct 3 2018
+# CTI-110 P2HW2 - Male Female Percentage
+# Xzavian Evans
+#
+
+males = int(input("Number of males"))
+females = int(input("Number of females"))
+totalstudents = males + females
+
+malePercentage = (males/totalstudents)*100
+femalePercentage = (females/totalstudents)*100
+
+print(malePercentage)
+print(femalePercentage)
